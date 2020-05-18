d0eca22a077fe5a61b4a80e5a690a858cf2a4912
Ralphilius <npt.ralph@gmail.com>
2020-05-18 17:08:59 +0700

    Added autoscroll

--- chromium.js
+++ chromium.js
@@ -12,9 +12,35 @@ async function getScreenshot(url, type, quality, fullPage) {
     await page.goto(url, {
         waitUntil: 'networkidle0'
     });
+    await page.setViewport({
+        width: 1200,
+        height: 800
+    });
+
+    await autoScroll(page);
+
     const file = await page.screenshot({ type,  quality, fullPage });
     await browser.close();
     return file;
+}
+
+async function autoScroll(page){
+    await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if(totalHeight >= scrollHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
 }
 
 module.exports = { getScreenshot };
